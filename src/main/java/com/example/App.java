162f145942c473e763de988550ffb62db94448bf
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-20 19:10:37 +0200

    JavaFx Scenes Drawing Staff

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,15 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,36 +30,78 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        
-        //Root node
+    public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        //Scene that show to user as a page
-        Scene scene = new Scene(root, Color.BLACK);
-        //Adding Icon to stage
-        Image icon = new Image("home.png");
-        stage.getIcons().add(icon);
-        //Adding title to stage
-        stage.setTitle("Stage 1");
+        Scene scene = new Scene(root, 600, 600, Color.SKYBLUE);
+        Stage stage = new Stage();
 
-        //Setting width and height for stage
-        stage.setWidth(420);
-        stage.setHeight(420);
+        //Adding Text
+        Text text = new Text();
+        text.setText("WHOOOOOOOOOOOOA!!");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Verdana", 50));
+        text.setFill(Color.GREEN);
+        
+        //Adding Line
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(200);
+        line.setStroke(Color.RED);
+        line.setStrokeWidth(5);
+        line.setOpacity(0.5);
+        line.setRotate(45);
 
-        //Enable and Disable Resizable for stage
-        // stage.setResizable(false);
+        //Adding Rectangle
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(100);
+        rectangle.setY(100);
+        rectangle.setWidth(100);
+        rectangle.setHeight(100);
+        rectangle.setStrokeWidth(5);
+        rectangle.setStroke(Color.BLACK);
+        rectangle.setFill(Color.TRANSPARENT);
 
-        //Set X and Y axis for stage
-        // stage.setX(50);
-        // stage.setY(50);
+        //Adding Triangle
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+            200.0,200.0,
+            300.0,300.0,
+            200.0, 300.0
+        );
+        triangle.setFill(Color.YELLOW);
+        triangle.setStroke(Color.BLACK);
+        triangle.setStrokeWidth(5);
 
-        //Set Fullscreen, Fullscreen exit hint and Fullscreen exit key combination
-        stage.setFullScreen(true);
-        stage.setFullScreenExitHint("You Can't Escape unless you didn't press q");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q")); 
+        //Adding Circle
+        Circle circle = new Circle();
+        circle.setCenterX(350);
+        circle.setCenterY(350);
+        circle.setRadius(50);
+        circle.setFill(Color.ORANGE);
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeWidth(5);
 
-        // Stage stage = new Stage();
-        //Stage is the window of the application
+
+        //Adding Image
+        Image image = new Image("pizza.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setX(400);
+        imageView.setY(400);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
+        root.getChildren().add(triangle);
+        root.getChildren().add(circle);
+        root.getChildren().add(imageView);
+
+
         stage.setScene(scene);
         stage.show();
     }
