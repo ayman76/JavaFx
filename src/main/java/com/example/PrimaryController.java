a8f6022051c53faf9b269e1cc4127f93e58108ba
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-21 22:15:23 +0200

    JavaFX ColorPicker

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,25 @@
 package com.example;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private AnchorPane myPane;
+
+    @FXML
+    private ColorPicker myColorPicker;
+
+    public void changeColor(ActionEvent event){
+        Color color = myColorPicker.getValue();
+        myPane.setBackground(new Background(new BackgroundFill(color, null, null)));
     }
 }
