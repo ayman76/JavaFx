cc90b624551a36d287480d75366eb980b80c52dc
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-21 22:01:39 +0200

    JavaFX RadioButtons

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,27 @@
 package com.example;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private RadioButton pizzaRadio, sushiRadio, ramenRadio;
+
+    public void getFood(ActionEvent exActionEvent){
+        if(pizzaRadio.isSelected()){
+            myLabel.setText(pizzaRadio.getText());
+        }else if(sushiRadio.isSelected()){
+            myLabel.setText(sushiRadio.getText());
+        }else if(ramenRadio.isSelected()){
+            myLabel.setText(ramenRadio.getText());
+        }
     }
 }
