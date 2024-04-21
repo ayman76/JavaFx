5095a92be228b56bde3b190d28394d67c03273af
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-21 22:09:47 +0200

    JavaFX DatePicker

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,25 @@
 package com.example;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private DatePicker myDatePicker;
+
+    public void selectDate(ActionEvent event){
+        LocalDate date = myDatePicker.getValue();
+        String myFormattedDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        myLabel.setText(myFormattedDate);
     }
 }
