205581403a7fae5ee58300ffe2f8697e26ac8d87
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-21 21:51:24 +0200

    JavaFX CheckBox

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,35 @@
 package com.example;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private CheckBox myCheckBox;
+
+    @FXML
+    private ImageView myImageView;
+
+    Image lampOff = new Image(getClass().getResourceAsStream("lampOFF.png"));
+    Image lampOn = new Image(getClass().getResourceAsStream("lampON.png"));
+
+    public void change(ActionEvent actionEvent){
+        if(myCheckBox.isSelected()){
+            myLabel.setText("ON");
+            myImageView.setImage(lampOn);
+        }else{
+            myLabel.setText("OFF");
+            myImageView.setImage(lampOff);
+        }
     }
 }
