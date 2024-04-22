942622ddfcac2e23946e6bf56a918e4c8e9ebae4
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-22 21:46:49 +0200

    JavaFx ChoiceBox

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,34 @@
 package com.example;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private ChoiceBox<String> myChoicebox;
+
+    private String[] food = { "Pizza", "Sushi", "Ramen" };
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        myChoicebox.getItems().addAll(food);
+        myChoicebox.setOnAction(this::getFood);
     }
+
+    public void getFood(ActionEvent event){
+        String selectedFood = myChoicebox.getValue();
+        myLabel.setText(selectedFood);
+    }
+
 }
