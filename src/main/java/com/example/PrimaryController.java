37ea55fd954452883aaa6b104d14937a220d6e2b
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-22 22:01:19 +0200

    JavaFX Slider

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,40 @@
 package com.example;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+public class PrimaryController implements Initializable{
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private Slider mySlider;
+
+    private int myTemprature;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        myTemprature = (int) mySlider.getValue();
+        myLabel.setText(Integer.toString(myTemprature) + "°C");
+
+        mySlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+               myTemprature = (int) mySlider.getValue();
+               myLabel.setText(Integer.toString(myTemprature) + "°C");
+            }
+                
+        });
     }
+
+    
 }
