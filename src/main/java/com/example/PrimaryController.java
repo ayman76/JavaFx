34c63f98a32ccb805e72660544ae3d2ff4a66eeb
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-04-22 22:47:06 +0200

    JavaFX ProgressBar

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,42 @@
 package com.example;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label myLabel;
+
+    @FXML
+    private ProgressBar myProgressBar;
+
+    @FXML
+    private Button myButton;
+
+    BigDecimal progress = new BigDecimal(String.format("%.2f", 0.0));
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        myProgressBar.setStyle("-fx-accent: #00FF00");
+    }
+
+    public void increaseProgress(ActionEvent event) throws InterruptedException {
+        if (progress.doubleValue() < 1) {
+            progress = new BigDecimal(String.format("%.2f", progress.doubleValue() + 0.1));
+            System.out.println(progress.doubleValue());
+            myProgressBar.setProgress(progress.doubleValue());
+            myLabel.setText(Double.toString(progress.doubleValue() * 100) + "%");
+        }
+
     }
 }
